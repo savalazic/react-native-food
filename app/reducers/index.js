b17db151bff19918a0cdc2eafbf8f798ad58d41c
savalazic <savalazic9@gmail.com>
2017-07-31 15:53:29 +0200

    chore: continue setting up

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from 'redux';
 import * as recipesReducer from './recipes';
 
 export default combineReducers(Object.assign(
-  recipesReducer,
+  recipesReducer
 ));
--- index.ios.js
+++ index.ios.js
@@ -2,19 +2,36 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
+import { createLogger } from 'redux-logger';
+
+import reducer from './app/reducers'
+
+const loggerMiddleware = createLogger({ predicate: (getState, action) => __DEV__ });
+
+function configureStore(initialState) {
+  const enhancer = compose( // bunch of different middlewares
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware,
+    ),
+  );
+
+  return createStore(reducer, initialState, enhancer);
+}
+
+const store = configureStore({});
+
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 
-export default class ReactNativeFood extends Component {
+class Food extends Component {
   render() {
     return (
-      <View style={styles.container}> 
-        <Text style={styles.welcome}>
+      <View> 
+        <Text>
           Welcome
         </Text>
       </View>
@@ -22,18 +39,10 @@ export default class ReactNativeFood extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
-});
+const App = () => (
+  <Provider store={store}>
+    <Food />
+  </Provider>
+);
 
-AppRegistry.registerComponent('ReactNativeFood', () => ReactNativeFood);
+AppRegistry.registerComponent('ReactNativeFood', () => App);
